9479d4bd353e7dfb7abfda14100ae82cba5ebaec
Nicolas Guth <nicolas.guth@gmail.com>
2020-01-17 15:30:52 +0100

    first commit
    Few modifications from first commit talk
    
    Amend due to a forgotten file add

---
+++ station_distance.rb
@@ -0,0 +1,68 @@
+require 'csv'
+require 'open-uri'
+
+def fetch_stations(url)
+  url = 'stations.csv' if File.file?('stations.csv')
+  csv_text = open(url)
+  CSV.parse(csv_text, headers: true, col_sep: ';')
+end
+
+def to_rad(angle)
+  angle.to_f * 0.017453292519943295
+end
+
+def distance_calc(coord)
+  a1 = Math.sin(coord[3] - coord[1])
+  a2 = Math.cos(coord[3]) * Math.cos(coord[1])
+  a3 = Math.sin(coord[2] - coord[0])
+  a = a1**2 + a2 * a3**2
+  b = Math.sqrt(a) / Math.sqrt(1 - a)
+  c = Math.atan(b)
+  (6371 * c).to_i
+end
+
+def station_user_selector(station_array)
+  station = []
+  p 'please select departure station id'
+  station << station_array[gets.chomp.to_i]
+  p 'please select arrival station id'
+  station << station_array[gets.chomp.to_i]
+end
+
+def station_select(station_list)
+  station_array = []
+  station_list.each_with_index do |row, index|
+    station_array << { id: index,
+                       name: row[1],
+                       latitude: row[5],
+                       longitude: row[6],
+                       country: row[8],
+                       is_main_st: row[11] }
+    break if index >= 50
+  end
+  station_array
+end
+
+def coordinates_buider(station)
+  long1 = to_rad(station[0][:longitude].to_f)
+  lat1 = to_rad(station[0][:latitude].to_f)
+  long2 = to_rad(station[1][:longitude].to_f)
+  lat2 = to_rad(station[1][:latitude].to_f)
+  [long1, lat1, long2, lat2]
+end
+
+def distance_interface
+  p 'Downloading list of stations, please wait...'
+  station_list = fetch_stations('https://raw.githubusercontent.com/trainline-eu/stations/master/stations.csv')
+  station_array = station_select(station_list)
+  station_array.each { |st| p "#{st[:id]} - #{st[:name]}" }
+  station = station_user_selector(station_array)
+  coordinates = coordinates_buider(station)
+  p '---------------------------------------------'
+  p "#{station[0][:name]} and #{station[1][:name]}\
+ are separated by #{distance_calc(coordinates)} km"
+  p '---------------------------------------------'
+end
+# station_list = fetch_stations('https://raw.githubusercontent.com/trainline-eu/stations/master/stations.csv')
+# p station_select(station_list)
+# distance_interface
---
+++ station_neighbours.rb
@@ -0,0 +1,70 @@
+require './station_distance.rb'
+
+def neighbour_interface
+  p 'Downloading list of stations, please wait...'
+  station_list = fetch_stations('https://raw.githubusercontent.com/trainline-eu/stations/master/stations.csv')
+  country_display(station_list)
+  p 'Quel est le pays dont les gares vous intéressent ?'
+  country = gets.chomp.downcase
+  station_array = station_country(country, station_list)
+  p 'entrez le 2 premières lettres de la gare recherchée'
+  short_list = prefix_choice(station_array)
+  station = central_station_selector(short_list)
+  p "quelle rayon maxi pour les gares aux alentours de #{station[0][:name]} ?"
+  radius = gets.chomp.to_i
+  station_array = stations_neighbour_selector(station_array, station, radius)
+  p "Gares dans un rayon de #{radius} km autour de #{station[0][:name]} :"
+  station_array.each { |st| p st[:name] }
+end
+
+def station_country(country, station_list)
+  station_array = []
+  station_list.each_with_index do |row, index|
+    if row[8].downcase == country
+      station_array << { id: index + 1, name: row[1], latitude: row[5], longitude: row[6], country: row[8], main_st: row[11] }
+    end
+  end
+  station_array
+end
+
+def distance_calc(coord)
+  lx = Math.cos(coord[1]) * (coord[2] - coord[0]) * 6371
+  ly = (coord[3] - coord[1]) * 6371
+  Math.sqrt(lx**2 + ly**2)
+end
+
+def country_display(station_list)
+  country_list = []
+  station_list.each { |st| country_list << st[8].downcase }
+  country_list.uniq.each { |country| p country }
+end
+
+def prefix_choice(station_array)
+  prefix = gets.chomp.downcase
+  short_list = station_array.select do |st|
+    st[:name][0, 2].downcase == prefix
+  end
+  short_list.each_with_index do |st, index|
+    st[:id] = index
+    p "#{st[:id]} - #{st[:name]}"
+  end
+  short_list
+end
+
+def central_station_selector(short_list)
+  # attention, il faut que les :id correspondent aux indices de tableau
+  p "Donnez l'indice de la gare souhaitée"
+  station = []
+  station_id = gets.chomp.to_i
+  station << short_list[station_id]
+end
+
+def stations_neighbour_selector(station_array, station, radius)
+  station_array.keep_if do |st|
+    station[1] = st
+    distance_calc(coordinates_buider(station)) < radius
+  end
+  station_array
+end
+
+# neighbour_interface
